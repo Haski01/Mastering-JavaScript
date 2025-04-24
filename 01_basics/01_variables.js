70641a8634fd5d562cae951f9d29fd36a010e581
Asad <153982814+Haski01@users.noreply.github.com>
2025-04-24 07:47:39 +0000

    another updation

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,16 +1,15 @@
 const accountId = 1144556
 let accountEmail = "asad@gmail.com"
 var accountPassword = "115577889"
-accountCity = "Mumbai"
+accountCity = "Mumbai" // not good practice to declare value without dataType like this
 let accountState;
 
 // accountId = 2 // not allowed to change variable value in which declared by 'const' keyword
+// console.log(accountId);
 
-accountEmail = "hc@hc.com"
+accountEmail = "ad@ad.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
+accountCity = "Meerut"
 
 /*
 Prefer not to use var
