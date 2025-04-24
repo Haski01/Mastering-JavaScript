056bcd39e9124c8f2082c0c33fc63fc3ab5136c3
Asad <153982814+Haski01@users.noreply.github.com>
2025-04-24 08:17:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version (not compulsary to write this, byDefault javaScript runs in strict mode)
+
+// alert( 3 + 3) // through error bcz we are using nodejs, not browser (other syntex to write alert)
+
+console.log(3 
+    +
+     3) // code readability should be high (not good practice to write like this)
+
+console.log("Asad") // this is good practice to write newly code start form next line...
+
+
+let name = "asad"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 // range for store value in number type 
+// bigint => similer like "double" (high capacity to store value)
+// string => ""
+// boolean => true/false
+// null => called standalone value
+// undefined => 
+// symbol => use for unique value
+
+
+// object
+
+console.log(typeof undefined); // output undefined
+console.log(typeof null); // output object
