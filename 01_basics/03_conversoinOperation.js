5e7fca616c3ace3c50d2159940047f82a036a7f9
Asad <153982814+Haski01@users.noreply.github.com>
2025-04-25 06:36:00 +0000

    Operations

--- 01_basics/03_conversoinOperation.js
+++ 01_basics/03_conversoinOperation.js
@@ -36,38 +36,43 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); // output => -3
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// console.log(2+2);  // output => 4
+// console.log(2-2);   // output => 0
+// console.log(2*2);  // output => 4
+// console.log(2**3); // output => 8
+// console.log(2/3); // output => 0.66666666
+// console.log(2%3); // output => 2
 
 let str1 = "hello"
-let str2 = "asad"
+let str2 = " asad"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); // output => hello asad
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); // output => 12
+// console.log(1 + "2"); // output => 12
+// console.log("1" + 2 + 2); // output => 122 
+// console.log(1 + 2 + "2"); // output => 32
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3); // output => 2
+// console.log( (3 + 4) * (5 % 3) ); // output => 14
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); // output => 1 (means to boolean conversion)
+// console.log(+""); // output => 0 
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
+// console.log(num1,num2,num3); // output => 4 4 4
+
 
 let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter); // output => 100
 ++gameCounter;
-console.log(gameCounter);
+// console.log(gameCounter); // output => 102
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
