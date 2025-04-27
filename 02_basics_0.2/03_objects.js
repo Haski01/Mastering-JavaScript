e652fc69c2bdeba50eec896977056918509cadcc
Asad <asadshah1066@gmail.com>
2025-04-27 12:50:59 +0530

    object part 1

---
+++ 02_basics_0.2/03_objects.js
@@ -0,0 +1,40 @@
+// [imp points]
+// > when we create object using "literals" then "singleton never create"
+// > or when we create object using "Constructor" then "singleton create"
+
+// singleton/ constructor
+// Object.create => contructor method
+
+//************************* <Object literals> **************************
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "Asad",
+  "full name": "Mohammad Asad",
+  [mySym]: "mykey1", // excess symbol
+  age: 18,
+  location: "Jaipur",
+  email: "asad@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]); // pass value as string bcz key convert into string in behind or auto..
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email = "asad@chatgpt.com";
+// Object.freeze(JsUser); // unable to change any value after freezing object
+// JsUser.email = "asad@microsoft.com";
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS user");
+};
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS user, ${this.name}`);
+};
+
+JsUser.greeting();
+JsUser.greetingTwo();
